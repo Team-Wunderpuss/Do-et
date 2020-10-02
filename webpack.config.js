85416f2370e684eb658ac44cd039873814d2aef9
Matt Jones <matt@Matts-MBP.fios-router.home>
2020-10-02 19:34:12 -0400

    fixed webpack port issues, now works on 3000 and 8080

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: process.env.NODE_ENV,
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "src/public"),
-    port: 8080,
-    publicPath: "http://localhost:3000/dist",
-    // hotOnly: true,
-    proxy: {
-      '/': {
-				target: 'http://localhost:3000/',
-				secure: false,
-			},
+module.exports = (env) => {
+  return {
+    entry: './src/index.js',
+    mode: process.env.NODE_ENV,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          options: { presets: ['@babel/env'] }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+      ],
     },
-  },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      //publicPath: "/dist/",
+      filename: 'bundle.js',
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "src/public"),
+      port: 8080,
+      publicPath:  "http://localhost:3000/dist", // '/dist/',
+      // hotOnly: true,
+      proxy: {
+        '/': {
+          target: 'http://localhost:3000/',
+          secure: false,
+        },
+      },
+    },
   // plugins: [new webpack.HotModuleReplacementPlugin()]
+  };
 };
