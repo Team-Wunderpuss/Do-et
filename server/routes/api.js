42af72fa8d8dfbcf9d90d533542f9bf8b081c7a6
Jin Soo Lim <jinsoolim1@gmail.com>
2020-10-03 08:19:29 -0700

    john added api + kyle changes

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,9 @@
 const fetch = require('node-fetch');
 const express = require('express');
 const router = express.Router();
+const userController = require("../controllers/userController");
+
+
 
 let city = 'los angeles'
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_APIKEY}`;
@@ -17,5 +20,38 @@ router.get('/', (req, res) => {
       res.send(data)
     })
 })
+
+// router.get("/", async (req, res, next) => {
+//   console.log('API router fired');
+//   try {
+//     const data = await userController.createUser(req, res, next);
+//     res.status(200).json({users: res.locals.users});
+//   } catch {
+//     console.error("error");
+//   }
+// });
+
+
+
+
+// create/validate new user
+router.post('/', userController.createUser, (req, res) => {
+  // const users = await userController.createUser(req, res, next);
+  res.status(200).json({ user: res.locals.user });
+});
+//   async (req, res, next) => {
+//     try {
+//       const users = await userController.validateUser(req, res, next);
+//       next();
+//     } catch (err) {
+//       return next({
+//         log: `api/user: ERROR ${err}`,
+//         message: { err: "something went terribly wrong :(" }
+//       });
+//     }
+//   },
+// );
+
+
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const userController = require('./controllers/userController');
+// const userController = require('./controllers/userController');
 
 const app = express();
 
